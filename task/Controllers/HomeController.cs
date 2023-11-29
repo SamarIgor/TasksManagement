ec96ac272319c529292b5b5cd33512a8e6961f1b
IgorSamar <46316672+IgorSamar@users.noreply.github.com>
2023-11-29 18:23:43 +0100

    Tasks to do on Home page

--- task/Controllers/HomeController.cs
+++ task/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using task.Models;
+using task.Data;
+using System.Security.Claims;
 
 namespace task.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly TaskContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,TaskContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
+    [Authorize]
     public IActionResult Index()
     {
-        return View();
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var tasks = _context.Tasks.Where(t => t.TaskForId == userId).ToList();
+        return View(tasks);
     }
 
     public IActionResult Privacy()
